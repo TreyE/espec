9fe00cc30938cd79000fbbc0f122752536572138
Trey <lewis.r.evans@gmail.com>
2008-12-05 17:53:36 -0500

    Some of that needed attribution I mentioned earlier.

--- lib/espec/src/sin_es_build.erl
+++ lib/espec/src/sin_es_build.erl
@@ -1,36 +1,24 @@
 %% -*- mode: Erlang; fill-column: 132; comment-column: 118; -*-
 
-% TODO: Properly attribute and describe this module.
+% TODO: Properly edoc this module.
 
 %%%-------------------------------------------------------------------
+%%% Copyright (c) 2008-2009 Lewis R. Evans IV
+%%%-------------------------------------------------------------------
+%%%-------------------------------------------------------------------
 %%% Copyright (c) 2006, 2007 Erlware
-%%%
-%%% Permission is hereby granted, free of charge, to any
-%%% person obtaining a copy of this software and associated
-%%% documentation files (the "Software"), to deal in the
-%%% Software without restriction, including without limitation
-%%% the rights to use, copy, modify, merge, publish, distribute,
-%%% sublicense, and/or sell copies of the Software, and to permit
-%%% persons to whom the Software is furnished to do so, subject to
-%%% the following conditions:
+%%%-------------------------------------------------------------------
 %%%
 %%% The above copyright notice and this permission notice shall
 %%% be included in all copies or substantial portions of the Software.
 %%%
-%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-%%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-%%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-%%% OTHER DEALINGS IN THE SOFTWARE.
 %%%---------------------------------------------------------------------------
-%%% @author Eric Merritt
+%%% @author Lewis R. Evans IV
 %%% @doc
-%%%  Supports building individual erl files in an application
+%%%  Supports building individual erl files in an application, as well as the
+%%%  modules in the application's 'test' directory.
 %%% @end
-%%% @copyright (C) 2007, Erlware
+%%% @copyright (C) 2008-2009, Lewis R. Evans IV 
 %%%--------------------------------------------------------------------------
 -module(sin_es_build).
 
